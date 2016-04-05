d50f24625c4dd4e65ac1f0af668bfc503d98cfff
DaydayUp <yxt9415@hotmail.com>
2016-04-05 01:23:57 -0400

    Initial Commit

---
+++ getDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  getDemoTH
+//
+//  Created by Daydayup-Yi on 4/5/16.
+//  Copyright © 2016 Daydayup-Yi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
